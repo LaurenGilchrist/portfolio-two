976b2cd1ac52b9a965ff1ce3c47deff28738d0de
LaurenGilchrist <laurengilchrist@hotmail.com>
2018-07-08 17:20:04 -0400

    new upload

---
+++ script.js
@@ -0,0 +1,73 @@
+const footer = document.querySelector('footer');
+footer.addEventListener('mouseover', function (){
+  footer.classList.add('hover');
+})
+footer.addEventListener('mouseout', function (){
+  footer.classList.remove('hover');    
+});
+
+
+(function() {
+
+  setUpPage();
+
+  function setUpPage() {
+    addSliderEventListeners();
+  }
+
+
+function addSliderEventListeners() {
+
+    const sliderPrev = document.getElementById('slider-prev');
+    const sliderNext = document.getElementById('slider-next');
+    const sliderFrame = document.getElementById('slider-frame');
+    const sliderCaption = document.getElementById('slider-caption');
+
+    const slides =
+      Array.from(sliderFrame.querySelectorAll('div[data-src]'))
+        .map(div => {
+          return {
+            imageUrl: div.getAttribute('data-src'),
+            caption: div.textContent.trim(),
+          };
+        });
+
+    let sliderIndex = 0;
+    displaySliderImage();
+
+    function displaySliderImage() {
+      let { imageUrl, caption } = slides[sliderIndex];
+      sliderFrame.style.backgroundImage = `url('${imageUrl}')`;
+      let count = `(${sliderIndex+1}/${slides.length}) `;
+      sliderCaption.innerHTML = count + caption;
+      sliderCaption.classList.add('flash');
+      setTimeout(() => {
+        sliderCaption.classList.remove('flash');
+      }, 0);
+    }
+
+    function displayPrevSliderImage() {
+      sliderIndex--;
+      if (sliderIndex < 0) {
+        sliderIndex = slides.length - 1;
+      }
+      displaySliderImage();
+    }
+
+    function displayNextSliderImage() {
+      sliderIndex++;
+      if (sliderIndex === slides.length) {
+        sliderIndex = 0;
+      }
+      displaySliderImage();
+    }
+
+    sliderPrev.addEventListener('click', displayPrevSliderImage);
+    sliderNext.addEventListener('click', displayNextSliderImage);
+
+    setInterval(function() {
+      displayNextSliderImage();
+    }, 4000);
+  }
+
+})();
